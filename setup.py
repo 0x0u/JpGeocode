7be7645a34d32615926cfea4aaa22446f139140a
m0zu <m0zurillex@gmail.com>
2019-01-05 23:48:27 +0900

    Release v0.3.1 🎍

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='JpGeocode',
-    version='0.1',
+    version='0.3.1',
     description='Search coordinates from municipalities in Japan',
     url='https://github.com/0x0u/JpGeocode',
     author='m0zu',
@@ -10,7 +10,10 @@ setup(
     license='MIT',
     keywords='Python3 japan geocoding',
     packages=find_packages(),
+    package_data={'JpGeocode':['data/jp_geocode.json']},
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     classifiers=['Programming Language :: Python :: 3.6']
 )
+
+
